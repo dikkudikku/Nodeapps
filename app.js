dde84066e2a5eab25fd9d570a42ba01d135a8cfd
Satish Srivastava <satish29@in.ibm.com>
2020-05-08 18:09:05 +0530

    added poryt for openshift

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-const port = process.env.PORT || 3000;
+const port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
+//const ip = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '0.0.0.0';
 app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port :',port);
 });
